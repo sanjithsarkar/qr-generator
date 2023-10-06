afd07e0313376c5d0dc32dec8f7ddafe268acb85
sanjithsarkar <sanjithsarkar2@gmail.com>
2023-10-07 04:12:33 +0600

    MyView included with class

--- includes/Views/MyView.php
+++ includes/Views/MyView.php
@@ -1,111 +1,100 @@
 <?php
 
-//include_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php');
+namespace WPPluginWithVueTailwind\Views;
+class UserProfileView
+{
+    private $wpdb;
 
-$db_host = 'localhost';
-$db_username = 'root';
-$db_password = '12345678';
-$db_name = 'qr_generator';
+    public function __construct()
+    {
+        global $wpdb;
+        $this->wpdb = $wpdb;
+    }
 
-$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+    public function renderProfileView($id)
+    {
+        $table_name = $this->wpdb->prefix . 'userdata';
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+        $columns = ['id', 'qr_name', 'name', 'surname', 'title', 'email', 'mobile', 'address', 'image'];
 
-$id = get_query_var('qr_id');
+        $sql = $this->wpdb->prepare("SELECT " . implode(', ', $columns) . " FROM $table_name WHERE id = %d", $id);
+        $results = $this->wpdb->get_results($sql);
 
-$query = "SELECT * FROM wp_userdata WHERE id = $id";
-$result = mysqli_query($conn, $query);
+        if (count($results) > 0) {
+            $viewResults = $results[0];
+            $image_url = WPM_URL . 'assets/images/' . $viewResults->image;
+        } else {
+            $viewResults = null;
+            $image_url = ''; // Set a default image URL or handle it as needed
+        }
 
-if (!$result) {
-    die("Error: " . mysqli_error($conn));
-}
+        // HTML rendering code
+        ?>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
-if (mysqli_num_rows($result) > 0) {
-
-    $user_data = mysqli_fetch_assoc($result);
-//    echo "<p>User ID: " . $user_data['user_id'] . "</p>";
-} else {
-    echo "User not found";
-}
-
-// Close the database connection
-mysqli_close($conn);
-?>
-
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
-    <div class="bg-white py-8">
-        <div class="max-w-screen-md mx-auto">
-            <div class="bg-white shadow-lg rounded-lg">
-                <div class="bg-black text-white py-4 px-6 rounded-t-lg">
-                    <h3 class="text-2xl font-semibold">View Profile</h3>
-                </div>
-                <div class="py-10">
-                    <div class="grid grid-cols-5">
-                        <div class="col-span-1">
-
-                        </div>
-                        <div class="col-span-3 flex justify-center items-center border-collapse border border-gray-300">
-                            <div class="flex justify-center">
-                                <div class="w-full py-6">
-                                    <div class=" rounded-full p-2 mx-auto">
-
-                                        <img src="<?php echo WPM_URL . 'assets/images/' . $user_data['image']; ?>"
-                                             alt="No Image" class="block w-32 h-32 mx-auto rounded-full">
-
-                                    </div>
-                                    <div class="text-center mt-4">
-                                        <span class="text-lg font-semibold"><?php echo $user_data['name']; ?></span>
+        <div class="bg-white py-8">
+            <div class="max-w-screen-md mx-auto">
+                <div class="bg-white shadow-lg rounded-lg">
+                    <div class="bg-black text-white py-4 px-6 rounded-t-lg">
+                        <h3 class="text-2xl font-semibold">View Profile</h3>
+                    </div>
+                    <div class="py-10">
+                        <div class="grid grid-cols-5">
+                            <div class="col-span-1"></div>
+                            <div class="col-span-3 flex justify-center items-center border-collapse border border-gray-300">
+                                <div class="flex justify-center">
+                                    <div class="w-full py-6">
+                                        <div class=" rounded-full p-2 mx-auto">
+                                            <img src="<?php echo $image_url; ?>" alt="No Image"
+                                                 class="block w-32 h-32 mx-auto rounded-full">
+                                        </div>
+                                        <div class="text-center mt-4">
+                                            <span class="text-lg font-semibold"><?php echo $viewResults->name; ?></span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-span-1">
-
-                        </div>
-                    </div>
-
-                    <div class="grid grid-cols-5">
-                        <div class="col-span-1">
-
+                            <div class="col-span-1"></div>
                         </div>
 
-                        <div class="col-span-3 flex justify-center items-center mt-4 border-collapse border border-gray-300">
-                            <div class="w-full">
-                                <table class="w-full table-fixed">
-                                    <tbody>
-                                    <tr class="bg-gray-100">
-                                        <td class="w-1/2 py-2 px-4 font-semibold pl-12">Name</td>
-                                        <td class="w-1/2 py-2 px-4"><?php echo $user_data['name']; ?></td>
-                                    </tr>
-                                    <tr class="bg-gray-50">
-                                        <td class="w-1/2 py-2 px-4 font-semibold pl-12">Email</td>
-                                        <td class="w-1/2 py-2 px-4"><?php echo $user_data['email']; ?></td>
-                                    </tr>
-                                    <tr class="bg-gray-100">
-                                        <td class="w-1/2 py-2 px-4 font-semibold pl-12">Mobile</td>
-                                        <td class="w-1/2 py-2 px-4"><?php echo $user_data['mobile']; ?></td>
-                                    </tr>
-                                    <tr class="bg-gray-50">
-                                        <td class="w-1/2 py-2 px-4 font-semibold pl-12">Address</td>
-                                        <td class="w-1/2 py-2 px-4"><?php echo $user_data['address']; ?></td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                        <div class="grid grid-cols-5">
+                            <div class="col-span-1"></div>
+                            <div class="col-span-3 flex justify-center items-center mt-4 border-collapse border border-gray-300">
+                                <div class="w-full">
+                                    <table class="w-full table-fixed">
+                                        <tbody>
+                                        <tr class="bg-gray-100">
+                                            <td class="w-1/2 py-2 px-4 font-semibold pl-12">Name</td>
+                                            <td class="w-1/2 py-2 px-4"><?php echo $viewResults->name; ?></td>
+                                        </tr>
+                                        <tr class="bg-gray-50">
+                                            <td class="w-1/2 py-2 px-4 font-semibold pl-12">Email</td>
+                                            <td class="w-1/2 py-2 px-4"><?php echo $viewResults->email; ?></td>
+                                        </tr>
+                                        <tr class="bg-gray-100">
+                                            <td class="w-1/2 py-2 px-4 font-semibold pl-12">Mobile</td>
+                                            <td class="w-1/2 py-2 px-4"><?php echo $viewResults->mobile; ?></td>
+                                        </tr>
+                                        <tr class="bg-gray-50">
+                                            <td class="w-1/2 py-2 px-4 font-semibold pl-12">Address</td>
+                                            <td class="w-1/2 py-2 px-4"><?php echo $viewResults->address; ?></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                        </div>
-
-
-                        <div class="col-span-1">
+                            <div class="col-span-1"></div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
-    </div>
+        <?php
+    }
+}
 
-
-<?php //echo get_query_var('qr_id') ?>
+// Usage
+$userProfileView = new UserProfileView();
+$id = get_query_var('qr_id');
+$userProfileView->renderProfileView($id);
+?>
